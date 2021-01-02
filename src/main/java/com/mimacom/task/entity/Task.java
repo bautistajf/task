06b5858474b2c23063f8d1978ab6b182467badde
Bautista <javier.bautista@flexit.es>
2021-01-02 21:51:45 +0100

    Update ID column type.

--- src/main/java/com/mimacom/task/entity/Task.java
+++ src/main/java/com/mimacom/task/entity/Task.java
@@ -20,7 +20,7 @@ import lombok.NoArgsConstructor;
 public final class Task {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name")
